da5056cd1bfa4a0f73c56d88824fc2c61a36913f
황주영 <165615814+H-JUYEONG@users.noreply.github.com>
2025-10-31 01:27:20 +0900

    Update main3.py

--- test/main3.py
+++ test/main3.py
@@ -6,7 +6,7 @@ model = YOLO("yolov8n.pt")
 # Train the model
 train_results = model.train(
     data="coco8.yaml",  # path to dataset YAML
-    epochs=150,  # number of training epochs
+    epochs=100,  # number of training epochs
     imgsz=640,  # training image size
     device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
 )
